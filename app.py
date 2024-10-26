96808ac339e55a788cbc3e95e785b643b1f11ce6
MuhammadPGA <muhammad.pourgholamali@gmail.com>
2024-10-26 19:44:04 +0300

    Made it look better

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import sys
 pygame.init()
 
 # Constants
-WIDTH, HEIGHT = 300, 300
+WIDTH, HEIGHT = 600, 600
 LINE_WIDTH = 15
 BOARD_ROWS = 3
 BOARD_COLS = 3
@@ -14,14 +14,15 @@ CIRCLE_RADIUS = SQUARE_SIZE // 3
 CIRCLE_WIDTH = 15
 CROSS_WIDTH = 25
 SPACE = SQUARE_SIZE // 4
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
+BACKGROUND_COLOR = (28, 170, 156)
+LINE_COLOR = (23, 145, 135)
+CIRCLE_COLOR = (239, 231, 200)
+CROSS_COLOR = (84, 84, 84)
 
 # Screen
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Tic Tac Toe")
-screen.fill(WHITE)
+screen.fill(BACKGROUND_COLOR)
 
 # Board
 board = [[None] * BOARD_COLS for _ in range(BOARD_ROWS)]
@@ -30,19 +31,19 @@ board = [[None] * BOARD_COLS for _ in range(BOARD_ROWS)]
 def draw_lines():
     # Horizontal lines
     for row in range(1, BOARD_ROWS):
-        pygame.draw.line(screen, BLACK, (0, row * SQUARE_SIZE), (WIDTH, row * SQUARE_SIZE), LINE_WIDTH)
+        pygame.draw.line(screen, LINE_COLOR, (0, row * SQUARE_SIZE), (WIDTH, row * SQUARE_SIZE), LINE_WIDTH)
     # Vertical lines
     for col in range(1, BOARD_COLS):
-        pygame.draw.line(screen, BLACK, (col * SQUARE_SIZE, 0), (col * SQUARE_SIZE, HEIGHT), LINE_WIDTH)
+        pygame.draw.line(screen, LINE_COLOR, (col * SQUARE_SIZE, 0), (col * SQUARE_SIZE, HEIGHT), LINE_WIDTH)
 
 def draw_figures():
     for row in range(BOARD_ROWS):
         for col in range(BOARD_COLS):
             if board[row][col] == 'O':
-                pygame.draw.circle(screen, BLACK, (col * SQUARE_SIZE + SQUARE_SIZE // 2, row * SQUARE_SIZE + SQUARE_SIZE // 2), CIRCLE_RADIUS, CIRCLE_WIDTH)
+                pygame.draw.circle(screen, CIRCLE_COLOR, (col * SQUARE_SIZE + SQUARE_SIZE // 2, row * SQUARE_SIZE + SQUARE_SIZE // 2), CIRCLE_RADIUS, CIRCLE_WIDTH)
             elif board[row][col] == 'X':
-                pygame.draw.line(screen, BLACK, (col * SQUARE_SIZE + SPACE, row * SQUARE_SIZE + SQUARE_SIZE - SPACE), (col * SQUARE_SIZE + SQUARE_SIZE - SPACE, row * SQUARE_SIZE + SPACE), CROSS_WIDTH)
-                pygame.draw.line(screen, BLACK, (col * SQUARE_SIZE + SPACE, row * SQUARE_SIZE + SPACE), (col * SQUARE_SIZE + SQUARE_SIZE - SPACE, row * SQUARE_SIZE + SQUARE_SIZE - SPACE), CROSS_WIDTH)
+                pygame.draw.line(screen, CROSS_COLOR, (col * SQUARE_SIZE + SPACE, row * SQUARE_SIZE + SQUARE_SIZE - SPACE), (col * SQUARE_SIZE + SQUARE_SIZE - SPACE, row * SQUARE_SIZE + SPACE), CROSS_WIDTH)
+                pygame.draw.line(screen, CROSS_COLOR, (col * SQUARE_SIZE + SPACE, row * SQUARE_SIZE + SPACE), (col * SQUARE_SIZE + SQUARE_SIZE - SPACE, row * SQUARE_SIZE + SQUARE_SIZE - SPACE), CROSS_WIDTH)
 
 def check_winner(player):
     # Vertical win
@@ -67,20 +68,20 @@ def check_winner(player):
 
 def draw_vertical_winning_line(col, player):
     posX = col * SQUARE_SIZE + SQUARE_SIZE // 2
-    pygame.draw.line(screen, RED, (posX, 15), (posX, HEIGHT - 15), LINE_WIDTH)
+    pygame.draw.line(screen, (0, 255, 0), (posX, 15), (posX, HEIGHT - 15), LINE_WIDTH)
 
 def draw_horizontal_winning_line(row, player):
     posY = row * SQUARE_SIZE + SQUARE_SIZE // 2
-    pygame.draw.line(screen, RED, (15, posY), (WIDTH - 15, posY), LINE_WIDTH)
+    pygame.draw.line(screen, (0, 255, 0), (15, posY), (WIDTH - 15, posY), LINE_WIDTH)
 
 def draw_ascending_diagonal(player):
-    pygame.draw.line(screen, RED, (15, HEIGHT - 15), (WIDTH - 15, 15), LINE_WIDTH)
+    pygame.draw.line(screen, (0, 255, 0), (15, HEIGHT - 15), (WIDTH - 15, 15), LINE_WIDTH)
 
 def draw_descending_diagonal(player):
-    pygame.draw.line(screen, RED, (15, 15), (WIDTH - 15, HEIGHT - 15), LINE_WIDTH)
+    pygame.draw.line(screen, (0, 255, 0), (15, 15), (WIDTH - 15, HEIGHT - 15), LINE_WIDTH)
 
 def restart():
-    screen.fill(WHITE)
+    screen.fill(BACKGROUND_COLOR)
     draw_lines()
     for row in range(BOARD_ROWS):
         for col in range(BOARD_COLS):
@@ -114,3 +115,4 @@ while True:
                 game_over = False
                 player = 'X'
     pygame.display.update()
+
